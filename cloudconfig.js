d17b1585b095d5f04730d52f5892f22b63b348a3
saisekhar070205 <ysaisekhar@gmail.com>
2025-12-17 21:46:26 +0530

    Downgrade cloudinary to fix Render build

--- cloudconfig.js
+++ cloudconfig.js
@@ -1,17 +1,17 @@
-const cloudinary = require("cloudinary").v2;
-const CloudinaryStorage = require("multer-storage-cloudinary");
+const cloudinary = require("cloudinary");
+const { CloudinaryStorage } = require("multer-storage-cloudinary");
 
 cloudinary.config({
-  cloud_name: process.env.CLOUD_NAME,
-  api_key: process.env.CLOUD_API_KEY,
-  api_secret: process.env.CLOUD_API_SECRET,
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
 const storage = new CloudinaryStorage({
   cloudinary,
   params: {
     folder: "wanderlust_DEV",
-    allowedFormats: ["jpg", "png", "jpeg"],
+    allowedFormats: ["jpg", "jpeg", "png"],
   },
 });
 
@@ -19,4 +19,3 @@ module.exports = {
   cloudinary,
   storage,
 };
-
